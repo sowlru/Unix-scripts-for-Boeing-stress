9c25f267939dde137f411f043b0c490ad4c880dc
Alex VK <aklimovskikh@yandex.ru>
2021-03-09 20:25:53 +0300

    add feadms-idtas

---
+++ feadms_idtas_tem/3_profile/s747_8f_s46_20080627_door_rev-30sep10.cc
@@ -0,0 +1,21 @@
+fd1  flaps down depart 1  
+fd2  flaps down depart 2  
+cr1  cruise 1             
+cv1  cruise 1 vert gust   
+ch1  cruise 1 hor gust    
+sv1  step climb 1 vert gus
+sh1  step climb 1 hor gust
+cr2  cruise 2             
+cv2  cruise 2 vert gust   
+ch2  cruise 2 hor gust    
+dv1  initial descent 1 (v)
+dh1  initial descent 1 (h)
+dv2  initial descent 2 (v)
+dh2  initial descent 2 (h)
+dv3  initial descent 3 (v)
+dh3  initial descent 3 (h)
+fa1  flaps down approach 1
+fa2  flaps down approach 2
+bsp  body gear spin up/spr
+wsp  wing gear spin up/spr
+zro  Cargo Door Op Cyc    
